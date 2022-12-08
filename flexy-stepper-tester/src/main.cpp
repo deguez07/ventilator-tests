880f920fc30f3297a2b8bde848a1fc4781b48c39
Diego Eguez <deguez07@yahoo.com>
2022-12-07 22:27:09 -0500

    Project base

---
+++ flexy-stepper-tester/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <FlexyStepper.h>
+
+
+void setup() {
+  Serial.println("Running flexy-stepper-tester");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
